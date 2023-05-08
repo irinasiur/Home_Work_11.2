5cc8022536176eef09006ea88f41cbe326446c00
irinasiur <drozdovairina10@gmail.com>
2023-05-08 22:16:34 +0300

    def capitalize_first_letter(a):

--- str_func.py
+++ str_func.py
@@ -1,5 +1,8 @@
 def uppercase_translation(a):
-    "Переводит буквы в верхний регистр" 
+    """Переводит буквы в верхний регистр"""
     return a.upper()
 
 
+def capitalize_first_letter(a):
+    """Переводит первую букву каждогомслова в строке в верхний регистр"""
+    return a.title()
